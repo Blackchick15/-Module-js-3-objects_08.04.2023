1595bf06d03117e69f3e7bac9aa463f36617965e
Oksana Petryshyn <petryshyn.v.oksana@gmail.com>
2023-04-07 13:56:08 +0300

    для роботи на уроці

---
+++ 01-2-methods.js
@@ -0,0 +1,10 @@
+/*
+ * Методи обєкта і this при зверненні до властивості в методах
+ *
+ * http://fecore.net.ua/books/m5ph3r-javascript/module-03/images/context.jpg
+ *
+ * - changeName
+ * - addTrack
+ * - updateRating
+ * - getTrackCount
+ */
---
+++ practice.js
@@ -0,0 +1,26 @@
+// Задача №1
+// Написати функцію capitalize яка буде приймати
+// строку і буде повертати нову де кожне слово буде з великої букви
+// capitalize('the quick brown fox') // 'The Quick Brown Fox '
+
+// function capitalize(string) {
+
+// }
+
+// console.log(capitalize("the quick brown fox"));
+
+// ...............
+// Задача №2
+// Напиши функції для роботи з колекцією навчальних курсів courses:
+
+// addCourse(name) - добвляє курс в кінець колекції
+// removeCourse(name) - видаляє курс з коллекції
+// updateCourse(oldName, newName)- змінює імя на нове
+
+const courses = ["HTML", "CSS", "JavaScript", "React", "PostgreSQL"];
+
+// addCourse("Express"); // ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'Express']
+// addCourse("CSS"); // 'У вас вже є такий курс'
+// removeCourse("React"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'Express']
+// removeCourse("Vue"); // 'Курс з таким імененем не знайдено'
+// updateCourse("Express", "NestJS"); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']

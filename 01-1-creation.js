4cc33356033e0637ddf3f73c1ab6b33e978a8ca3
Blackchick15 <109077737+Blackchick15@users.noreply.github.com>
2023-04-08 12:19:05 +0300

    commit

--- 01-1-creation.js
+++ 01-1-creation.js
@@ -4,7 +4,21 @@
  * - Ввластивість, ключі (імя) і значення
  * - Як відрізнити обєкт від області видимостф
  */
+const playlist = {
+    name:'Мій супер плей-лист',
+      rating: 5,
+      traks:['трек- 1 трек- 2'],
+      tracksCount: 3,
+};
 
+console.log(playlist.name);
+const x={
+
+}
+// const fn = function (myOjekt){
+
+// }
+// fn( a: 1, )
 /*
  * Доступ до влвстивості
  * - obj.key
